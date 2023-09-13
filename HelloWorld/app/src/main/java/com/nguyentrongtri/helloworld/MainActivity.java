1ceefbd3d1af5ec87855ff726461a88493945a92
tri63132732 <trongtri903@gmail.com>
2023-09-13 15:18:00 +0700

    Update MainActivity.java

--- HelloWorld/app/src/main/java/com/nguyentrongtri/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/nguyentrongtri/helloworld/MainActivity.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    public static void main(String[] args){
-        System.out.println("Hello world");
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
