10d6c7d6d3991abdd4a309450776107e2fcfd9fe
tri63132732 <trongtri903@gmail.com>
2023-10-11 15:03:01 +0700

    Say hello

--- SayHello/app/src/main/java/com/nguyentrongtri/sayhello/MainActivity.java
+++ SayHello/app/src/main/java/com/nguyentrongtri/sayhello/MainActivity.java
@@ -4,19 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText_Name;
+    Button buttonSayHi, buttonXinChao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+    void findViews(){
+        // Step 1: find name, sẵn để sau này dùng
+        //Ở các điều khiển khác nhau
+        editText_Name= findViewById(R.id.name);
+        buttonSayHi = findViewById(R.id.btnHello);
+        buttonXinChao=findViewById(R.id.btnHello1);
     }
     public void XuLyNoiHello(View v){
-        // Step 1: find name
-        EditText editText_Name= findViewById(R.id.name);
+
         // Step 2
             // Get string
         String strname = editText_Name.getText().toString();
@@ -24,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,strThongBao,Toast.LENGTH_LONG).show();
     }
     public void XuLyNoiHello1(View v){
-        // Step 1: find name
-        EditText editText_Name= findViewById(R.id.name);
         // Step 2
         // Get string
         String strname = editText_Name.getText().toString();
