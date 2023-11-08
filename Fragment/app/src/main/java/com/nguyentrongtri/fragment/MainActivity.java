685234ae0c16e3cf24df4b0450962ab3332593eb
tri63132732 <trongtri903@gmail.com>
2023-11-08 16:13:26 +0700

    add

---
+++ Fragment/app/src/main/java/com/nguyentrongtri/fragment/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nguyentrongtri.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
